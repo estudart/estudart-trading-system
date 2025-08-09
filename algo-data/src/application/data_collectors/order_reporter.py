74d0a2981c2821c27a30d939fdf558a1ae5c308d
hdxestudart <erico.studartdf@gmail.com>
2025-08-09 11:34:08 -0300

    feat: add order reporter

---
+++ algo-data/src/application/data_collectors/order_reporter.py
@@ -0,0 +1,35 @@
+import time
+import logging
+
+from src.application.data_collectors.data_collector import DataCollector
+from src.infrastructure.adapters.websocket_adapter import WebsocketAdapter
+from src.infrastructure.adapters.queue.redis_adapter import RedisAdapter
+
+
+class OrderReporter(DataCollector):
+    def __init__(
+        self,
+        logger: logging.Logger,
+        reporter_adapter: WebsocketAdapter,
+        redis_adapter: RedisAdapter
+    ):
+        self.logger = logger
+        self.reporter_adapter = reporter_adapter
+        self.redis_adapter = redis_adapter
+
+    def dispatch_order_report_event(self, processed_message_data: dict):
+        channel = f"order-{processed_message_data['StrategyId']}"
+        self.redis_adapter.publish_message(channel, processed_message_data)
+        self.logger.info(f"{channel} | Order report event was dispatched: {processed_message_data}")
+
+    def start_collecting(self):
+        while True:
+            try:
+                ws = self.reporter_adapter.get_ws(self.dispatch_order_report_event)
+                ws.run_forever()
+            except Exception:
+                self.logger.info("WebSocket disconnected. Reconnecting in 5 seconds...")
+                time.sleep(5)
+
+    def run(self):
+        self.start_collecting()
