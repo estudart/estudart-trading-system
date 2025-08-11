baa8342c966e137c778e8d6761e2a37c7f5ffeb0
hdxestudart <erico.studartdf@gmail.com>
2025-08-11 10:43:41 -0300

    fix: simplify logic with only one dict

--- algo-data/src/application/data_collectors/md_data_collector.py
+++ algo-data/src/application/data_collectors/md_data_collector.py
@@ -22,48 +22,65 @@ class MdDataCollector(DataCollector):
         self.inav_adapter = inav_adapter
         self.message_broker = message_broker
         self.retry_time = retry_time
-        self.onshore_offshore_mapping = {
-            "BITH11": {
-                "offshore": "HBTC.BH",
-                "underlying_asset": "BTCUSD_PERP"
-            },
-            "ETHE11": {
-                "offshore": "HETH.BH",
-                "underlying_asset": "ETHUSD_PERP"
-            },
-            "SOLH11": {
-                "offshore": "HSOL.BH",
-                "underlying_asset": "SOLUSD_PERP"
-            }
-        }
         self.from_underlying_to_etf = {
-            "BTCUSD_PERP": "BITH11",
-            "ETHUSD_PERP": "ETHE11",
-            "SOLUSD_PERP": "SOLH11"
+            "BTCUSD_PERP": {
+                "onshore": "BITH11",
+                "offshore": "HBTC.BH"
+            },
+            "ETHUSD_PERP": {
+                "onshore": "ETHE11",
+                "offshore": "HETH.BH"
+            },
+            "SOLUSD_PERP": {
+                "onshore": "SOLH11",
+                "offshore": "HSOL.BH"
+            },
+        }
+        self.inav_price_dict = {}
+
+    def should_publish_data(self, symbol, inav):
+        if not self.inav_price_dict.get(symbol):
+            return True
+        
+        if inav != self.inav_price_dict[symbol]:
+            return True
+        
+        return False
+
+    def mount_message_data(self, asset: str, inav: float, amount_of_underlying_asset: float):
+        return {
+            "symbol": asset,
+            "inav": inav,
+            "amount_of_underlying_asset": amount_of_underlying_asset
         }
     
-    def stream_data(self, provider: str, asset: str, price: float):
-        onshore = self.from_underlying_to_etf[asset]
-        offshore = self.onshore_offshore_mapping[onshore]["offshore"]
+    def publish_data(self, provider: str, asset: str, price: float):
+        onshore = self.from_underlying_to_etf[asset]["onshore"]
+        offshore = self.from_underlying_to_etf[asset]["offshore"]
 
         qty = self.inav_adapter.get_crypto_quantity_on_onshore_etf(onshore, offshore)
         dollar_price = float(self.message_broker.get_key("USD:BRL"))
         inav = round(qty * price * dollar_price, 2)
 
-        self.logger.info(f"INAV | {provider}-{asset}: {inav}")
+        if self.should_publish_data(onshore, inav):
+            self.inav_price_dict[onshore] = inav
+        else:
+            return
 
         channel = f"inav-{onshore}-{provider}"
-        self.message_broker.publish_message(channel, inav)
+        self.logger.info(f"{channel}: {inav}")
+        message_data = self.mount_message_data(onshore, inav, qty)
+        self.message_broker.publish_message(channel, message_data)
 
     def start_websocket_session(self):
         self.logger.info(f"Starting websocket connection...")
-        ws = self.websocket_adapter.get_ws(self.stream_data)
+        ws = self.websocket_adapter.get_ws(self.publish_data)
         ws.run_forever()
     
     def start_collecting(self):
         self.logger.info(f"Caching underlying quantity on ETFs...")
-        for k, v in self.onshore_offshore_mapping.items():
-            self.inav_adapter.get_crypto_quantity_on_onshore_etf(k, v["offshore"])
+        for k, v in self.from_underlying_to_etf.items():
+            self.inav_adapter.get_crypto_quantity_on_onshore_etf(v["onshore"], v["offshore"])
 
         while True:
             try:
